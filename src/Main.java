edf92712129ef137b69602872abba03f140f6a6d
devoyo <121737425+devoyoo@users.noreply.github.com>
2025-05-29 19:28:21 +1200

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("heeeelllooooo world.");
 		System.out.println("Heeeelooo again");
+		
+		int number = 7;
+		
+		System.out.println("Heres a number: " + number);
+		
 	}
+	
 	
 }
